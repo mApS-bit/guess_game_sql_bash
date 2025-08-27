262fef70065c0cace0180692dffd02dc4c3dd575
mApS-bit <psmarco832@gmail.com>
2025-08-26 21:53:50 -0400

    feat: frequent users prompt added

--- number_guess.sh
+++ number_guess.sh
@@ -15,7 +15,14 @@ PSQL='psql --username=freecodecamp --dbname=number_guess -t --no-align -c'
 IS_NEW_USER=$($PSQL "SELECT user_name FROM users WHERE user_name = '$USERNAME'")
 
 if [[ -z $IS_NEW_USER ]]; then
+  $PSQL "INSERT INTO users(user_name) VALUES('$USERNAME')"
   echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+else 
+  USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE user_name = '$USERNAME'")
+  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 echo "Guess the secret number between 1 and 1000:"
+
