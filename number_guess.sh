31c7a20d542795ead7dca31be2de82bcb954a0d0
mApS-bit <psmarco832@gmail.com>
2025-08-26 21:34:14 -0400

    feat: Prompt for user guess

--- number_guess.sh
+++ number_guess.sh
@@ -6,7 +6,7 @@ read USERNAME
 echo $USERNAME
 #generates a random number
 
-SECRET_NUMBER=$(( RANDOM ))
+SECRET_NUMBER=$(( 1 + RANDOM % 1000 ))
 echo $SECRET_NUMBER
 
 #conect to database
@@ -17,3 +17,5 @@ IS_NEW_USER=$($PSQL "SELECT user_name FROM users WHERE user_name = '$USERNAME'")
 if [[ -z $IS_NEW_USER ]]; then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 fi
+
+echo "Guess the secret number between 1 and 1000:"
