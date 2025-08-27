08f561760a4072c863eb102d3066556d2d63cd25
mApS-bit <psmarco832@gmail.com>
2025-08-26 22:27:47 -0400

    fix: ok

--- number_guess.sh
+++ number_guess.sh
@@ -3,11 +3,11 @@
 #prompt for username
 echo " Enter your username: "
 read USERNAME
-echo $USERNAME
+#echo $USERNAME
 #generates a random number
 
 SECRET_NUMBER=$(( 1 + RANDOM % 1000 ))
-echo $SECRET_NUMBER
+#echo $SECRET_NUMBER
 
 #conect to database
 PSQL='psql --username=freecodecamp --dbname=number_guess -t --no-align -c'
@@ -23,6 +23,8 @@ else
   IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
-
+#       Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
 
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
